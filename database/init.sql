f45a0fd9fcf8f30faddfb2d51c194330a280cc53
LauraLG2000 <l.labartagonzalez@gmail.com>
2026-04-22 19:31:33 +0200

    Se ha añadido la tabla de servicio dentro de la base de datos. Se ha separado el campo fecha de las citas en dos atributos distintos, uno de tipo date y otro de tipo time. Se ha sustituido la clave primaria de la tabla have_allergies

--- database/init.sql
+++ database/init.sql
@@ -2,37 +2,37 @@ create database if not exists vettion;
 use vettion;
 
 -- Tabla de dueños.
-create table if not exists dueno (
-    dni varchar(9) not null primary key,
+create table if not exists owner (
+    dni varchar(9) primary key,
     name varchar(100) not null,
     surname varchar(100) not null,
     phone varchar(15) not null,
-    email varchar(100) not null
+    email varchar(100)
 );
 
 -- Tabla de alergias
-create table if not exists alergia (
+create table if not exists allergy (
     id int auto_increment primary key,
     name varchar(100) not null,
     description varchar(255) not null
 );
 
 -- Tabla de veterinarios.
-create table if not exists veterinario (
-    dni varchar(9) not null primary key,
+create table if not exists veterinarian (
+    dni varchar(9) primary key,
     name varchar(100) not null,
     surname varchar(100) not null,
     phone varchar(15) not null,
     address varchar(100) not null,
     num_SS varchar(100) not null,
-    num_colegiado varchar(100) not null,
+    num_collegiate varchar(100) not null,
     email varchar(100) not null, 
-    speciality varchar(100) not null
+    speciality varchar(100)
 );
 
 -- Tabla de empleados de la limpieza.
-create table if not exists empleado_limpieza (
-    dni varchar(9) not null primary key,
+create table if not exists cleaner (
+    dni varchar(9) primary key,
     name varchar(100) not null, 
     surname varchar(100) not null,
     phone varchar(15) not null,
@@ -42,7 +42,7 @@ create table if not exists empleado_limpieza (
 );
 
 -- Tabla de mascotas
-create table if not exists mascota (
+create table if not exists pet (
     id int auto_increment primary key,
     name varchar(100) not null,
     type varchar(100) not null,
@@ -51,55 +51,69 @@ create table if not exists mascota (
     sex varchar(20) not null,
     age int not null,
 
-    dueno_dni varchar(9),
-    constraint fk_dueno_mascota
-        foreign key (dueno_dni) references dueno(dni) on delete cascade
+    owner_dni varchar(9),
+    constraint fk_owner_pet
+        foreign key (owner_dni) references owner(dni) on delete cascade
 );
 
 -- Tabla intermedia de alergia y mascotas
-create table if not exists tener_alergia (
-    alergia_id int,
-    mascota_id int,
-    primary key (alergia_id, mascota_id),
-    constraint fk_alergia_tener_alergia
-        foreign key (alergia_id) references alergia (id) on delete cascade,
-    constraint fk_mascota_tener_alergia
-        foreign key (mascota_id) references mascota (id) on delete cascade
+create table if not exists have_allergy (
+    new_allergy_pet int auto_increment primary key,
+    description text,
+
+    allergy_id int,
+    pet_id int,
+    constraint fk_allergy_have_allergy
+        foreign key (allergy_id) references allergy (id) on delete cascade,
+    constraint fk_pet_have_allergy
+        foreign key (pet_id) references pet (id) on delete cascade
 );
 
 -- Tabla de salas
-create table if not exists sala (
+create table if not exists room (
     id int auto_increment primary key,
     type varchar(100) not null,
     name varchar(100) not null,
-    disponibility boolean not null default true
+    disponibility boolean default true
 );
 
--- Tabla intermedia de salas y empleados de limpieza
-create table if not exists programacion_limpieza (
-    id_programacion int auto_increment primary key,
-    limpiador_dni varchar(9),
-    sala_id int,
-    fecha_limpieza datetime not null,
-    constraint fk_limpiador_sala
-        foreign key (limpiador_dni) references empleado_limpieza (dni) on delete cascade,
-    constraint fk_sala_limpieza
-        foreign key (sala_id) references sala (id) on delete cascade
+-- Tabla de servicios
+create table if not exists service (
+    id_service int auto_increment primary key,
+    name_service varchar(100) not null,
+    price decimal(7,2) not null,
+    description text
 );
 
--- Tabla de programar la cita
-create table if not exists programar_cita (
-    id_cita int auto_increment primary key,
-    mascota_id int,
-    sala_id int,
-    veterinario_dni varchar(9),
-    fecha_hora datetime not null,
-    precio decimal(10,2) not null,
-    motivo varchar(255),
-    constraint fk_cita_mascota
-        foreign key (mascota_id) references mascota(id),
-    constraint fk_cita_sala 
-        foreign key (sala_id) references sala(id),
-    constraint fk_cita_veterinario
-        foreign key (veterinario_dni) references veterinario(dni)
+-- Tabla de programar servicio de limpieza
+create table if not exists clean_service (
+    id_clean_service int auto_increment primary key,
+    day date not null,
+    hour_start time,
+
+    cleaner_dni varchar(9),
+    room_id int,
+    constraint fk_cleaner_clean_service
+        foreign key (cleaner_dni) references cleaner (dni) on delete cascade,
+    constraint fk_room_clean_service
+        foreign key (room_id) references room (id) on delete cascade
+);
+
+-- Tabla de programar cita para la mascota
+create table if not exists appointment (
+    id_appointment int auto_increment primary key,
+    appointment_date date not null,
+    start_hour time,
+    duration int,
+    reason varchar(255),
+
+    pet_id int,
+    room_id int,
+    veterinarian_dni varchar(9),
+    constraint fk_pet_appointment
+        foreign key (pet_id) references pet(id) on delete cascade,
+    constraint fk_room_appointment 
+        foreign key (room_id) references room(id) on delete cascade,
+    constraint fk_veterinarian_appointment
+        foreign key (veterinarian_dni) references veterinarian(dni) on delete cascade
 );
