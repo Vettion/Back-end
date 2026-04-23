9d75d3a568804c277cfd72ccbee26ffabc123e34
7adrii <adriloma007@gmail.com>
2026-04-23 18:54:53 +0200

    Refactor routing setup for owner and pet management

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const cors = require('cors');
-const owner = require('./router/owner_router.js');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/', owner);
+// Rutas.
+const ownerRouter = require('./router/owner_router.js');
+const petRouter = require('./router/petRouter.js');
+
+// URLs base.
+app.use('/owner', ownerRouter);
+app.use('/pet', petRouter);
 
 app.listen(8080, () => {
     console.log("Iniciando el backend en el puerto 8080");
