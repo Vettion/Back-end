90a6f095b328898f8c233406c97626ee314a9ae1
7adrii <adriloma007@gmail.com>
2026-04-29 20:15:09 +0200

    feat: create ownerRouter to handle CRUD operations for pet owners

--- src/router/ownerRouter.js
+++ src/router/ownerRouter.js
@@ -1,13 +1,16 @@
+// Este archivo contiene la lista de todas las operaciones definidas en el /controller/ownerController.js
+
 const express = require('express');
 const router = express.Router();
 
-const {getOwners, getOwner, postOwner, putOwner, deleteOwner} = require('../controller/ownerController.js');
+const { getOwners, getOwner, postOwner, putOwner, deleteOwner } = require('../controller/ownerController.js');
+const { validateOwnerId, validateAddOwner, validateUpdateOwner } = require('../validators/owner.js')
 
 // Rutas para la gestion de dueños
 router.get('/vettion/owners', getOwners);
-router.get('/vettion/owners/:dni_owner', getOwner);
-router.post('/vettion/owners', postOwner);
-router.put('/vettion/owners/:dni_owner', putOwner);
-router.delete('/vettion/owners/:dni_owner', deleteOwner);
+router.get('/vettion/owners/:dni_owner', validateOwnerId, getOwner);
+router.post('/vettion/owners', validateAddOwner, postOwner);
+router.put('/vettion/owners/:dni_owner', validateUpdateOwner, putOwner);
+router.delete('/vettion/owners/:dni_owner', validateOwnerId, deleteOwner);
 
 module.exports = router;
