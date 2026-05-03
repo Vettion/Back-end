54332f328433c3656d1052ab08a45c1e27cb48c7
7adrii <adriloma007@gmail.com>
2026-05-03 15:28:06 +0200

    feat: initialize express application and configure base API routes

--- src/app.js
+++ src/app.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const cors = require('cors');
-const appointmentRouter = require('./router/appointmentRouter.js');
-const cleanServiceRouter = require('./router/cleanServiceRouter.js');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/appointments', appointmentRouter);
-app.use('/clean_services', cleanServiceRouter);
 // Rutas.
 const ownerRouter = require('./router/ownerRouter.js');
 const consultRouter = require('./router/consultRouter.js');
 const petRouter = require('./router/petRouter.js');
+const appointmentRouter = require('./router/appointmentRouter.js');
+const cleanServiceRouter = require('./router/cleanServiceRouter.js');
 
 // URLs base.
 app.use('/owners', ownerRouter);
 app.use('/consults', consultRouter);
 app.use('/pets', petRouter);
+app.use('/appointments', appointmentRouter);
+app.use('/clean_services', cleanServiceRouter);
 
 app.listen(8080, () => {
     console.log("Iniciando el backend en el puerto 8080");
